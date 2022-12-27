56f859d307dc2f6207e72836179d7cd8fa3d85bb
devteni <fatunmbiteniola001@gmail.com>
2022-12-27 19:35:35 +0100

    :bug: added response cookie options for client

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
+import moment from 'moment';
+import config from '../config/config';
+import logger from '../config/logger';
 import AuthService from '../services/auth.service';
 import TokenService from '../services/token.service';
 import UserService from '../services/user.service';
@@ -20,17 +23,23 @@ class AuthController {
    */
   public register = catchAsync(async (req: Request, res: Response) => {
     const newUser = await this.userService.createUser(req.body);
-    const tokens = await this.tokenService.generateAuthTokens(newUser);
+    const { access, refresh } = await this.tokenService.generateAuthTokens(newUser);
 
-    res.cookie('refresh-token', tokens.refresh.token, { maxAge: tokens.refresh.expires.getMilliseconds() });
+    res.cookie('refresh-token', refresh.token, {
+      expires: refresh.expires,
+      httpOnly: true,
+      secure: config.env === 'production',
+      sameSite: 'strict',
+    });
+
     return res.status(201).json({
       data: {
         _id: newUser._id,
         first_name: newUser.first_name,
         last_name: newUser.last_name,
         email: newUser.email,
-        ...tokens,
       },
+      tokens: { access },
     });
   });
 
@@ -43,17 +52,24 @@ class AuthController {
   // eslint-disable-next-line class-methods-use-this
   public login = catchAsync(async (req: Request, res: Response) => {
     const user = await this.authService.loginUser(req.body);
-    const tokens = await this.tokenService.generateAuthTokens(user);
+    const { access, refresh } = await this.tokenService.generateAuthTokens(user);
 
-    res.cookie('refresh-token', tokens.refresh.token, { maxAge: tokens.refresh.expires.getMilliseconds() });
-    return res.status(201).json({
+    logger.info(moment(refresh.expires).format('x'));
+    res.cookie('refresh-token', refresh.token, {
+      expires: refresh.expires,
+      httpOnly: true,
+      secure: config.env === 'production',
+      sameSite: 'strict',
+    });
+
+    return res.status(200).json({
       data: {
         _id: user._id,
         first_name: user.first_name,
         last_name: user.last_name,
         email: user.email,
-        ...tokens,
       },
+      tokens: { access },
     });
   });
 }
