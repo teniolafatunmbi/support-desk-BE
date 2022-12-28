f896c85b1bf2ebf379741713035e87df8ef5eb4d
devteni <fatunmbiteniola001@gmail.com>
2022-12-28 10:59:37 +0100

    (chore): implemented token blacklisting on failed auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { TokenExpiredError } from 'jsonwebtoken';
+import Token from '../models/token.model';
 import User from '../models/user.model';
 import TokenService from '../services/token.service';
 import ApiError from '../utils/ApiError';
@@ -24,12 +25,20 @@ const authGuard = catchAsync(async (req: Request, res: Response, next: NextFunct
         try {
           // use refresh token
           token = req.cookies['refresh-token'];
+          const tokenInDb = await Token.findOne({ token, blacklisted: false });
+
+          if (!tokenInDb) {
+            throw new ApiError(401, 'Unauthorized');
+          }
+
           const decodedToken = await new TokenService().decodeToken(token);
           if (typeof decodedToken !== 'string') {
             req.user = await User.findById(decodedToken.sub).select('-password');
           }
+
           next();
         } catch (err) {
+          await Token.updateOne({ token }, { blacklisted: true });
           throw new ApiError(401, 'Unauthorized');
         }
       } else {
